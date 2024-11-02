626395e544c36fb2f07305436ac234ef4794de31
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-02 11:19:45 +0200

    Testing: Accessing specific data inside response array

--- scripts/apis.js
+++ scripts/apis.js
@@ -1,14 +1,27 @@
-const instance = axios.create({baseURL: "https://www.world-wonders-api.org/v0/wonders"});
+const instance = axios.create({baseURL: "https://www.world-wonders-api.org/v0/wonders"})
 
 const fetchData = async () =>{
-    const response = await instance.get();
+    const response = await instance.get()
     const data = response.data
-    return data;
+    return data
 } 
 
-const getData = fetchData().then( (data) =>{
-    const firstElement = data[0]
-    console.log(firstElement.name)
+let build_year = null
+let categories = []
+let britannica = null
+let google_maps = null
+let images = []
+let trip_advisor = null
+let wiki = null
+let location_text = null
+let name = null
+let summary = null
+let time_period = null
+
+fetchData().then( (data) =>{
+    let arrayElement = data[0]
+    build_year = arrayElement.build_year
+    console.log(build_year)
 })
 
 
