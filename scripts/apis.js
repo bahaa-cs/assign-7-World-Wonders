7c4066110ae3512e17d5512ffd564eb5b4b6cfc3
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-02 10:43:11 +0200

    Testing: fetchData

--- scripts/apis.js
+++ scripts/apis.js
@@ -1,1 +1,10 @@
-console.log(true)
+const instance = axios.create({baseURL: "https://www.world-wonders-api.org/v0/wonders"});
+
+const fetchData = async () =>{
+    const response = await instance.get();
+    const data = response.data
+    console.log(data[0]);
+} 
+
+fetchData();
+
