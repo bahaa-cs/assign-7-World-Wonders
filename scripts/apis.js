c2b05531b43f9b3e7d0d7ada70f099e6f50424f2
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-02 12:13:28 +0200

    Storing response data values into variables

--- scripts/apis.js
+++ scripts/apis.js
@@ -14,14 +14,39 @@ let images = []
 let trip_advisor = null
 let wiki = null
 let location_text = null
-let name = null
+let name_text = null
 let summary = null
 let time_period = null
 
+let arrayElement = []
+
 fetchData().then( (data) =>{
-    let arrayElement = data[0]
+    arrayElement = data[0]
     build_year = arrayElement.build_year
+    categories = arrayElement.categories
+    britannica = arrayElement.links.britannica
+    google_maps = arrayElement.links.google_maps
+    images = arrayElement.links.images
+    trip_advisor = arrayElement.links.trip_advisor
+    wiki = arrayElement.links.wiki
+    location_text = arrayElement.location
+    name_text = arrayElement.name
+    summary = arrayElement.summary
+    time_period = arrayElement.time_period
+
     console.log(build_year)
+    console.log(categories)
+    console.log(britannica)
+    console.log(google_maps)
+    console.log(images)
+    console.log(trip_advisor)
+    console.log(wiki)
+    console.log(location_text)
+    console.log(name_text)
+    console.log(summary)
+    console.log(time_period)
+
+    
 })
 
 
